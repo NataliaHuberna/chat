6c0f5c8f799378e4e6868b189e40d14e3d0c47e8
NataliaHuberna <natalyahuberna@gmail.com>
2022-01-31 17:18:00 +0200

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
 import './App.css';
+import {
+    Routes,
+    Route,
+    // useRoutes,
+} from "react-router-dom";
+import SignIn from './Components/Common/SignIn/SignIn';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>Hello world!</p>
-            </header>
-        </div>
+        <Routes>
+            <Route path="/" element={<SignIn />} />
+            {/* <Route element={<MainLayout />}>
+              <Route path="/signUp" element={<SignUp />} />
+              <Route path="/asseptInvitation" element={<AsseptInvitation />} />
+              <Route path="/errorInvitation" element={<ErrorInvitation />} />
+              <Route path="/newPassword" element={<NewPassword />} />
+              <Route path="/forgotPassword" element={<ForgotPassword />} />
+              <Route path="/errorForgotPassword" element={<ErrorForgotPassword />} />
+            </Route>
+            <Route path="/maim" element={<Main />} /> */}
+        </Routes>
     );
 }
 
---
+++ src/Components/Common/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignIn= () => (
+    <div>       
+        <p className="sign-in">SignIn</p>        
+    </div>
+);
+
+export default SignIn;
